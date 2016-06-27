6fde143e436973f41c59a8411a9fc32825f64769
alexlittle <alex@digital-campus.org>
2016-06-27 08:32:33 +0300

    update

--- money/reports/views.py
+++ money/reports/views.py
@@ -55,45 +55,54 @@ def graph_view(request,currency='GBP'):
     
     balances = []
     
-    valuation_accounts = Valuation.objects.filter(account__type='invest').values_list('account_id').distinct()
-    
-    print valuation_accounts
-    
+    valuation_gbp_accounts = Valuation.objects.filter(account__type='invest', account__currency='GBP').values_list('account_id').distinct()
+    valuation_eur_accounts = Valuation.objects.filter(account__type='invest', account__currency='EUR').values_list('account_id').distinct()
+
     for i in range(96,-1,-1):
         balance = {} 
         old_date = now - dateutil.relativedelta.relativedelta(months=i)
         last_day = datetime.datetime(int(old_date.strftime("%Y")), int(old_date.strftime("%m")), 1, 23, 59, tzinfo=tz)\
                      + dateutil.relativedelta.relativedelta(day=1, months=+1, days=-1)
         
+        d = ExchangeRate.objects.filter(date__lte=last_day).order_by('-date')[:1]
+        ex_rate = d[0].rate
+        
         # non valuation GBP accounts
         transactions_gbp = Transaction.objects.filter(account__currency='GBP',date__lte=last_day, account__type='cash').\
-                                        exclude( account_id__in=valuation_accounts, payment_type='Transfer').\
+                                        exclude( account_id__in=valuation_gbp_accounts, payment_type='Transfer').\
                                         aggregate(sum_in=Sum('credit'),sum_out=Sum('debit'))
         non_valuation_gbp = transactions_gbp['sum_in']-transactions_gbp['sum_out'] 
         
         valuation_gbp = 0
         # valuation GBP accounts
-        for va in valuation_accounts:
+        for va in valuation_gbp_accounts:
             v = Valuation.objects.filter(account_id=va, date__lte=last_day).aggregate(max=Max('date'))
             value = Valuation.objects.filter(account_id=va,date=v['max'])
             if value:
                 valuation_gbp += value[0].value
         
+        
+        valuation_eur = 0
+        # valuation EUR accounts
+        for va in valuation_eur_accounts:
+            v = Valuation.objects.filter(account_id=va, date__lte=last_day).aggregate(max=Max('date'))
+            value = Valuation.objects.filter(account_id=va,date=v['max'])
+            if value:
+                valuation_eur += (value[0].value) * ex_rate
+                
         # non valuation EUR accounts
         transactions_eur = Transaction.objects.filter(account__currency='EUR',date__lte=last_day, account__type='cash').\
-                                        exclude(account_id__in=valuation_accounts, payment_type='Transfer').\
+                                        exclude(account_id__in=valuation_eur_accounts, payment_type='Transfer').\
                                         aggregate(sum_in=Sum('credit'),sum_out=Sum('debit'))
-         
-        non_valuation_eur = 0
-        d = ExchangeRate.objects.filter(date__lte=last_day).order_by('-date')[:1]
-        if d.count() > 0:
-            ex_rate = d[0].rate
+        
+        non_valuation_eur = 0 
+        if transactions_eur['sum_in'] is not None:
             non_valuation_eur = (transactions_eur['sum_in']-transactions_eur['sum_out']) * ex_rate
-            print non_valuation_eur
                                             
         balance['date'] = last_day
-        balance['total'] = non_valuation_gbp + valuation_gbp + non_valuation_eur
+        balance['total'] = non_valuation_gbp + valuation_gbp + non_valuation_eur + valuation_eur
         balance['valuation_gbp'] = valuation_gbp
+        balance['valuation_eur'] = valuation_eur
         balance['non_valuation_gbp'] = non_valuation_gbp
         balance['non_valuation_eur'] = non_valuation_eur
         
--- money/views.py
+++ money/views.py
@@ -23,20 +23,36 @@ def home_view(request):
       
       
       
-    accs_invest = Account.objects.filter(active=True, type='invest', currency='GBP').order_by('name')
-    total_invest = 0
-    balances_invest = []
-    for b in accs_invest:
+    accs_gbp_invest = Account.objects.filter(active=True, type='invest', currency='GBP').order_by('name')
+    total_gbp_invest = 0
+    balances_gbp_invest = []
+    for b in accs_gbp_invest:
         # get most recent valuation
         v_tmp = Valuation.objects.filter(account=b).aggregate(date=Max('date'))
-        valuation = Valuation.objects.get(account=b, date=v_tmp['date'])
-        acc = {}
-        acc['account'] = b
-        acc['balance'] = valuation.value
-        acc['date'] = v_tmp['date']
-        balances_invest.append(acc)
-        total_invest += valuation.value
-          
+        if v_tmp['date'] is not None:
+            valuation = Valuation.objects.get(account=b, date=v_tmp['date'])
+            acc = {}
+            acc['account'] = b
+            acc['balance'] = valuation.value
+            acc['date'] = v_tmp['date']
+            balances_gbp_invest.append(acc)
+            total_gbp_invest += valuation.value
+     
+    accs_eur_invest = Account.objects.filter(active=True, type='invest', currency='EUR').order_by('name')
+    total_eur_invest = 0
+    balances_eur_invest = []
+    for b in accs_eur_invest:
+        # get most recent valuation
+        v_tmp = Valuation.objects.filter(account=b).aggregate(date=Max('date'))
+        if v_tmp['date'] is not None:
+            valuation = Valuation.objects.get(account=b, date=v_tmp['date'])
+            acc = {}
+            acc['account'] = b
+            acc['balance'] = valuation.value
+            acc['date'] = v_tmp['date']
+            balances_eur_invest.append(acc)
+            total_eur_invest += valuation.value
+             
     balances_eur = Transaction.objects.filter(account__active=True, account__type='cash', account__currency='EUR').order_by('account__name').values('account').annotate(total_credit=Sum("credit"), total_debit=Sum("debit"))
     total_eur = 0
     for b in balances_eur:
@@ -44,16 +60,18 @@ def home_view(request):
         b['account'] = Account.objects.get(pk=b['account'])
         total_eur += b['balance']
      
-    pensions = Account.objects.filter(type='pension',active=True).order_by('name')
+    pensions_gbp = Account.objects.filter(type='pension',active=True, currency='GBP').order_by('name')
        
     return render_to_response('money/home.html',
                               {'balances_gbp': balances_gbp,
                                'total_gbp': total_gbp,
-                               'balances_invest': balances_invest,
-                               'total_invest': total_invest,
+                               'balances_gbp_invest': balances_gbp_invest,
+                               'total_gbp_invest': total_gbp_invest,
+                               'balances_eur_invest': balances_eur_invest,
+                               'total_eur_invest': total_eur_invest,
                                'balances_eur': balances_eur,
                                'total_eur': total_eur,
-                               'pensions': pensions,},
+                               'pensions': pensions_gbp,},
                               context_instance=RequestContext(request))
     
 def account_view(request, account_id):
