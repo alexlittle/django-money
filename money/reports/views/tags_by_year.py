ced2f9c46c32565ac331aeddae7055528e87c150
Alex Little <consult@alexlittle.net>
2023-10-06 08:12:15 +0200

    updates

--- money/reports/views/tags_by_year.py
+++ money/reports/views/tags_by_year.py
@@ -17,8 +17,16 @@ class TagsByYearView(TemplateView):
         except KeyError:
             year = datetime.datetime.today().year
 
+        years = []
+        for i in range(2022, datetime.datetime.today().year+1, 1):
+            years.append(i)
+
         context['year'] = year
+        context['years'] = years
         context['tags'] = Tag.objects.filter(transactiontag__transaction__date__year=year) \
             .values('id', 'name', 'category', year=F('transactiontag__transaction__date__year')) \
+            .annotate(sum_in=Sum('transactiontag__transaction__credit'), sum_out=Sum('transactiontag__transaction__debit'))
+        context['categories'] = Tag.objects.filter(transactiontag__transaction__date__year=year) \
+            .values('category', year=F('transactiontag__transaction__date__year')) \
             .annotate(sum_in=Sum('transactiontag__transaction__credit'), sum_out=Sum('transactiontag__transaction__debit'))
         return context
