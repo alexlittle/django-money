2515ea131bb60b7815996ed99c92af8aa76d70d8
Alex Little <consult@alexlittle.net>
2024-05-03 07:24:21 +0200

    update for budget

--- money/reports/views/budget.py
+++ money/reports/views/budget.py
@@ -2,7 +2,7 @@ from django.views.generic.list import ListView
 from django.views.generic import TemplateView
 from django.db.models import Sum
 
-from money.models import AccountingPeriod, Transaction, TransactionTag
+from money.models import AccountingPeriod, Transaction, TransactionTag, Tag
 
 class BudgetView(ListView):
     template_name = 'money/reports/budget.html'
@@ -13,13 +13,6 @@ class BudgetView(ListView):
 
 class BudgetByPeriodView(TemplateView):
     template_name = 'money/reports/budget_by_period.html'
-
-    def get_expenses(self, query, tag):
-         transactions = query.filter(transactiontag__tag__name=tag)
-         trans_ids = list(transactions.values_list('id', flat=True))
-         total = transactions.aggregate(total=Sum("debit"))['total'] if transactions.aggregate(total=Sum("debit"))['total'] else 0
-         return total, trans_ids
-
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -33,38 +26,40 @@ class BudgetByPeriodView(TemplateView):
                                     .exclude(transaction__payment_type="Transfer") \
                                     .exclude(tag__name="kollektiivi")
 
-        income_groups = income_transactions.values("tag__category").distinct()
+        income_groups = Tag.objects.exclude(name="kollektiivi").values('category').distinct().order_by('category')
         income = {}
         income_total = 0
         for ig in income_groups:
             group_income_total = 0
-            group_transactions = income_transactions.filter(tag__category=ig['tag__category'])
+            group_transactions = income_transactions.filter(tag__category=ig['category'])
             for gt in group_transactions:
+                print(gt.get_credit_in_base_currency())
                 group_income_total = group_income_total + gt.get_credit_in_base_currency()
             income_total = income_total + group_income_total
-            income[ig['tag__category']] = group_income_total
+            income[ig['category']] = group_income_total
 
         expense_transactions = TransactionTag.objects.filter(transaction__date__gte=accounting_period.start_date,
                                                              transaction__date__lte=accounting_period.end_date,
                                                              allocation_debit__gt=0) \
             .exclude(transaction__payment_type__in=("Transfer", "Cashpoint")) \
             .exclude(tag__name="kollektiivi")
+
         # Personal Expenses
         personal_expenses = []
         personal_expenses_total = 0
         # House & personal
         personal_transactions = expense_transactions.filter(tag__category__in=("house","personal"))
-        personal_expenses_tags = personal_transactions.values("tag__name", "tag__id").distinct().order_by('tag__name')
+        personal_expenses_tags = Tag.objects.filter(category__in=("house","personal")).values("name", "id").distinct().order_by('name')
         for pet in personal_expenses_tags:
             p_expense = {}
             tag_expense_total = 0
-            group_transactions = personal_transactions.filter(tag__name=pet['tag__name'])
+            group_transactions = personal_transactions.filter(tag__name=pet['name'])
             for gt in group_transactions:
                 tag_expense_total = tag_expense_total + gt.get_debit_in_base_currency()
             personal_expenses_total = personal_expenses_total + tag_expense_total
-            p_expense['name'] = pet['tag__name']
+            p_expense['name'] = pet['name']
             p_expense['total'] = tag_expense_total
-            p_expense['id'] = pet['tag__id']
+            p_expense['id'] = pet['id']
             personal_expenses.append(p_expense)
 
         # Car
@@ -95,18 +90,36 @@ class BudgetByPeriodView(TemplateView):
         business_expenses = []
         business_expenses_total = 0
         business_transactions = expense_transactions.filter(tag__category__in=("business", "design", "rental"))
-        business_expenses_tags = business_transactions.values("tag__name", "tag__id").distinct().order_by('tag__name')
+        business_expenses_tags = Tag.objects.filter(category__in=("business", "design", "rental")).values("name", "id").distinct().order_by('name')
         for bet in business_expenses_tags:
             b_expense = {}
             tag_expense_total = 0
-            group_transactions = business_transactions.filter(tag__name=bet['tag__name'])
+            group_transactions = business_transactions.filter(tag__name=bet['name'])
             for gt in group_transactions:
                 tag_expense_total = tag_expense_total + gt.get_debit_in_base_currency()
             business_expenses_total = business_expenses_total + tag_expense_total
-            b_expense['name'] = bet['tag__name']
+            b_expense['name'] = bet['name']
             b_expense['total'] = tag_expense_total
-            b_expense['id'] = bet['tag__id']
+            b_expense['id'] = bet['id']
             business_expenses.append(b_expense)
+
+        # Missing?
+        personal_ids = personal_transactions.values('transaction__id')
+        car_ids = car_transactions.values('transaction__id')
+        travel_ids = travel_transactions.values('transaction__id')
+        misc_ids = misc_transactions.values('transaction__id')
+        bus_ids = business_transactions.values('transaction__id')
+        missing_transactions = Transaction.objects.filter(date__gte=accounting_period.start_date,
+                                                          date__lte=accounting_period.end_date,
+                                                          debit__gt=0) \
+            .exclude(transactiontag__tag__name="kollektiivi") \
+            .exclude(account_id=49) \
+            .exclude(payment_type="Transfer") \
+            .exclude(id__in=personal_ids) \
+            .exclude(id__in=car_ids) \
+            .exclude(id__in=travel_ids) \
+            .exclude(id__in=misc_ids) \
+            .exclude(id__in=bus_ids)
 
         context['period'] = accounting_period
         context['income'] = income
@@ -117,4 +130,6 @@ class BudgetByPeriodView(TemplateView):
 
         context['business_expenses'] = business_expenses
         context['business_expenses_total'] = business_expenses_total
+
+        context['missing_transactions'] = missing_transactions
         return context
