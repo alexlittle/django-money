8e307b9c14685654f054e0b9ab62cb454955e2bb
Alex Little <alex@alexlittle.net>
2026-08-27 15:13:03 +0100

    update for naive date time

--- money/migrations/0010_add_quarters.py
+++ money/migrations/0010_add_quarters.py
@@ -4,88 +4,88 @@ from django.db import migrations
 
 QUARTERS = {
     "2019 Q4": {
-        "start_date": datetime.datetime(2019, 10, 1, 0, 0, 0),
-        "end_date": datetime.datetime(2019, 12, 31, 23, 59, 59),
+        "start_date": datetime.datetime(2019, 10, 1, 0, 0, 0, tzinfo=datetime.UTC),
+        "end_date": datetime.datetime(2019, 12, 31, 23, 59, 59, tzinfo=datetime.UTC),
     },
     "2020 Q1": {
-        "start_date": datetime.datetime(2020, 1, 1, 0, 0, 0),
-        "end_date": datetime.datetime(2020, 3, 31, 23, 59, 59),
+        "start_date": datetime.datetime(2020, 1, 1, 0, 0, 0, tzinfo=datetime.UTC),
+        "end_date": datetime.datetime(2020, 3, 31, 23, 59, 59, tzinfo=datetime.UTC),
     },
     "2020 Q2": {
-        "start_date": datetime.datetime(2020, 4, 1, 0, 0, 0),
-        "end_date": datetime.datetime(2020, 6, 30, 23, 59, 59),
+        "start_date": datetime.datetime(2020, 4, 1, 0, 0, 0, tzinfo=datetime.UTC),
+        "end_date": datetime.datetime(2020, 6, 30, 23, 59, 59, tzinfo=datetime.UTC),
     },
     "2020 Q3": {
-        "start_date": datetime.datetime(2020, 7, 1, 0, 0, 0),
-        "end_date": datetime.datetime(2020, 9, 30, 23, 59, 59),
+        "start_date": datetime.datetime(2020, 7, 1, 0, 0, 0, tzinfo=datetime.UTC),
+        "end_date": datetime.datetime(2020, 9, 30, 23, 59, 59, tzinfo=datetime.UTC),
     },
     "2020 Q4": {
-        "start_date": datetime.datetime(2020, 10, 1, 0, 0, 0),
-        "end_date": datetime.datetime(2020, 12, 31, 23, 59, 59),
+        "start_date": datetime.datetime(2020, 10, 1, 0, 0, 0, tzinfo=datetime.UTC),
+        "end_date": datetime.datetime(2020, 12, 31, 23, 59, 59, tzinfo=datetime.UTC),
     },
     "2021 Q1": {
-        "start_date": datetime.datetime(2021, 1, 1, 0, 0, 0),
-        "end_date": datetime.datetime(2021, 3, 31, 23, 59, 59),
+        "start_date": datetime.datetime(2021, 1, 1, 0, 0, 0, tzinfo=datetime.UTC),
+        "end_date": datetime.datetime(2021, 3, 31, 23, 59, 59, tzinfo=datetime.UTC),
     },
     "2021 Q2": {
-        "start_date": datetime.datetime(2021, 4, 1, 0, 0, 0),
-        "end_date": datetime.datetime(2021, 6, 30, 23, 59, 59),
+        "start_date": datetime.datetime(2021, 4, 1, 0, 0, 0, tzinfo=datetime.UTC),
+        "end_date": datetime.datetime(2021, 6, 30, 23, 59, 59, tzinfo=datetime.UTC),
     },
     "2021 Q3": {
-        "start_date": datetime.datetime(2021, 7, 1, 0, 0, 0),
-        "end_date": datetime.datetime(2021, 9, 30, 23, 59, 59),
+        "start_date": datetime.datetime(2021, 7, 1, 0, 0, 0, tzinfo=datetime.UTC),
+        "end_date": datetime.datetime(2021, 9, 30, 23, 59, 59, tzinfo=datetime.UTC),
     },
     "2021 Q4": {
-        "start_date": datetime.datetime(2021, 10, 1, 0, 0, 0),
-        "end_date": datetime.datetime(2021, 12, 31, 23, 59, 59),
+        "start_date": datetime.datetime(2021, 10, 1, 0, 0, 0, tzinfo=datetime.UTC),
+        "end_date": datetime.datetime(2021, 12, 31, 23, 59, 59, tzinfo=datetime.UTC),
     },
     "2022 Q1": {
-        "start_date": datetime.datetime(2022, 1, 1, 0, 0, 0),
-        "end_date": datetime.datetime(2022, 3, 31, 23, 59, 59),
+        "start_date": datetime.datetime(2022, 1, 1, 0, 0, 0, tzinfo=datetime.UTC),
+        "end_date": datetime.datetime(2022, 3, 31, 23, 59, 59, tzinfo=datetime.UTC),
     },
     "2022 Q2": {
-        "start_date": datetime.datetime(2022, 4, 1, 0, 0, 0),
-        "end_date": datetime.datetime(2022, 6, 30, 23, 59, 59),
+        "start_date": datetime.datetime(2022, 4, 1, 0, 0, 0, tzinfo=datetime.UTC),
+        "end_date": datetime.datetime(2022, 6, 30, 23, 59, 59, tzinfo=datetime.UTC),
     },
     "2022 Q3": {
-        "start_date": datetime.datetime(2022, 7, 1, 0, 0, 0),
-        "end_date": datetime.datetime(2022, 9, 30, 23, 59, 59),
+        "start_date": datetime.datetime(2022, 7, 1, 0, 0, 0, tzinfo=datetime.UTC),
+        "end_date": datetime.datetime(2022, 9, 30, 23, 59, 59, tzinfo=datetime.UTC),
     },
     "2022 Q4": {
-        "start_date": datetime.datetime(2022, 10, 1, 0, 0, 0),
-        "end_date": datetime.datetime(2022, 12, 31, 23, 59, 59),
+        "start_date": datetime.datetime(2022, 10, 1, 0, 0, 0, tzinfo=datetime.UTC),
+        "end_date": datetime.datetime(2022, 12, 31, 23, 59, 59, tzinfo=datetime.UTC),
     },
     "2023 Q1": {
-        "start_date": datetime.datetime(2023, 1, 1, 0, 0, 0),
-        "end_date": datetime.datetime(2023, 3, 31, 23, 59, 59),
+        "start_date": datetime.datetime(2023, 1, 1, 0, 0, 0, tzinfo=datetime.UTC),
+        "end_date": datetime.datetime(2023, 3, 31, 23, 59, 59, tzinfo=datetime.UTC),
     },
     "2023 Q2": {
-        "start_date": datetime.datetime(2023, 4, 1, 0, 0, 0),
-        "end_date": datetime.datetime(2023, 6, 30, 23, 59, 59),
+        "start_date": datetime.datetime(2023, 4, 1, 0, 0, 0, tzinfo=datetime.UTC),
+        "end_date": datetime.datetime(2023, 6, 30, 23, 59, 59, tzinfo=datetime.UTC),
     },
     "2023 Q3": {
-        "start_date": datetime.datetime(2023, 7, 1, 0, 0, 0),
-        "end_date": datetime.datetime(2023, 9, 30, 23, 59, 59),
+        "start_date": datetime.datetime(2023, 7, 1, 0, 0, 0, tzinfo=datetime.UTC),
+        "end_date": datetime.datetime(2023, 9, 30, 23, 59, 59, tzinfo=datetime.UTC),
     },
     "2023 Q4": {
-        "start_date": datetime.datetime(2023, 10, 1, 0, 0, 0),
-        "end_date": datetime.datetime(2023, 12, 31, 23, 59, 59),
+        "start_date": datetime.datetime(2023, 10, 1, 0, 0, 0, tzinfo=datetime.UTC),
+        "end_date": datetime.datetime(2023, 12, 31, 23, 59, 59, tzinfo=datetime.UTC),
     },
     "2024 Q1": {
-        "start_date": datetime.datetime(2024, 1, 1, 0, 0, 0),
-        "end_date": datetime.datetime(2024, 3, 31, 23, 59, 59),
+        "start_date": datetime.datetime(2024, 1, 1, 0, 0, 0, tzinfo=datetime.UTC),
+        "end_date": datetime.datetime(2024, 3, 31, 23, 59, 59, tzinfo=datetime.UTC),
     },
     "2024 Q2": {
-        "start_date": datetime.datetime(2024, 4, 1, 0, 0, 0),
-        "end_date": datetime.datetime(2024, 6, 30, 23, 59, 59),
+        "start_date": datetime.datetime(2024, 4, 1, 0, 0, 0, tzinfo=datetime.UTC),
+        "end_date": datetime.datetime(2024, 6, 30, 23, 59, 59, tzinfo=datetime.UTC),
     },
     "2024 Q3": {
-        "start_date": datetime.datetime(2024, 7, 1, 0, 0, 0),
-        "end_date": datetime.datetime(2024, 9, 30, 23, 59, 59),
+        "start_date": datetime.datetime(2024, 7, 1, 0, 0, 0, tzinfo=datetime.UTC),
+        "end_date": datetime.datetime(2024, 9, 30, 23, 59, 59, tzinfo=datetime.UTC),
     },
     "2024 Q4": {
-        "start_date": datetime.datetime(2024, 10, 1, 0, 0, 0),
-        "end_date": datetime.datetime(2024, 12, 31, 23, 59, 59),
+        "start_date": datetime.datetime(2024, 10, 1, 0, 0, 0, tzinfo=datetime.UTC),
+        "end_date": datetime.datetime(2024, 12, 31, 23, 59, 59, tzinfo=datetime.UTC),
     },
 }
 
